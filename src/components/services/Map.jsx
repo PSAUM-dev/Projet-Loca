352cf5381bc44d0b396a4889368e052a3fc59745
PSAUM <83640023+PSAUM-dev@users.noreply.github.com>
2025-11-03 12:23:33 +0100

    Update map interface & loading screen

--- src/components/services/Map.jsx
+++ src/components/services/Map.jsx
@@ -2,7 +2,7 @@ import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api"
 
 const containersStyle = {
     width: '100%',
-    height: '400px'
+    height: '100%'
 };
 
 const center = {
@@ -13,14 +13,24 @@ const center = {
 const Map = () => {
 
     const { isLoaded } = useLoadScript({
-        googleMapsApiKey: ""//import.meta.env.VITE_GOOGLE_MAP_API
+        googleMapsApiKey: import.meta.env.VITE_GOOGLE_MAP_API
     });
 
-    if (!isLoaded) return <div>Chargement</div>;
+    const options = {
+        zoomControl: false,
+        mapTypeControl: false,
+        streetViewControl: true,
+        fullscreenControl: false,
+        cameraControl : false
+    };
+
+    if (!isLoaded) return <div className="bg-gray-100 w-screen h-screen flex items-center justify-center">
+        <p className="ms-80"><span className="loading loading-spinner bg-[#4f46e5] w-10 h-10"></span></p>
+    </div>;
 
     return (
 
-        <GoogleMap mapContainerStyle={containersStyle} center={center} zoom={13}>
+        <GoogleMap mapContainerStyle={containersStyle} center={center} zoom={20} options={options}>
 
             <Marker position={center} />
 
